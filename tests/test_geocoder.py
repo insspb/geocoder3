d626b32788913ce8ac234adaaf49deb3c40ba83d
Andrey Shpak <ashpak@ashpak.ru>
2022-05-13 13:22:44 +0300

    Extend simple  location container tests

--- tests/test_geocoder.py
+++ tests/test_geocoder.py
@@ -1,3 +1,5 @@
+import pytest
+
 import geocoder
 
 
@@ -36,10 +38,25 @@ def test_entry_points():
     geocoder.ipfinder
 
 
-def test_location():
-    g = geocoder.location("45.4215296, -75.6971931")
+@pytest.mark.parametrize(
+    "location",
+    (
+        ("45.4215296, -75.6971931"),
+        {"y": 45.4215296, "x": -75.6971931},
+        {"lat": 45.4215296, "lng": -75.6971931},
+        [45.4215296, -75.6971931],
+    ),
+    ids=(
+        "Tuple input",
+        "Dictionary input",
+        "XY Dictionary input",
+        "List input",
+    ),
+)
+def test_location(location):
+    g = geocoder.Location(location)
     assert g.ok
-    g = geocoder.location({"lat": 45.4215296, "lng": -75.6971931})
-    assert g.ok
-    g = geocoder.location([45.4215296, -75.6971931])
-    assert g.ok
+    assert g.latlng == [45.4215296, -75.6971931]
+    assert g.latitude == 45.4215296
+    assert g.longitude == -75.6971931
+    assert g.xy == [-75.6971931, 45.4215296]
