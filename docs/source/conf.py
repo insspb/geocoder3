c4b2d2e661996f4a8087db4a9811255a02c7d752
Andrey Shpak <ashpak@ashpak.ru>
2022-05-13 11:18:16 +0300

    Fix docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import sys
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../.."))
 from geocoder import __version__  # noqa
 
 # -- General configuration ----------------------------------------------------
