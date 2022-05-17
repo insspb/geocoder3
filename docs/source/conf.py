df73f002f0fe2e4d8f3bae8c3e39129bc2e9e09d
Andrey Shpak <ashpak@ashpak.ru>
2022-05-18 02:41:56 +0300

    Documentation theme fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -140,6 +140,10 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_css_files = [
+    "css/custom.css",
+]
+html_style = "css/custom.css"
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 # html_last_updated_fmt = '%b %d, %Y'
