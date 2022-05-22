60327a0891316c1e153917cf61fe6996352fdbf2
Andrey Shpak <ashpak@ashpak.ru>
2022-05-23 02:43:18 +0300

    Merge mapbox and mapbox_reverse and move to core providers

--- geocoder/providers/addresses/__init__.py
+++ geocoder/providers/addresses/__init__.py
@@ -1,4 +1,3 @@
 # flake8: noqa
-from geocoder.providers.addresses.mapbox import MapboxQuery, MapboxResult
 from geocoder.providers.addresses.mapzen import MapzenQuery, MapzenResult
 from geocoder.providers.addresses.opencage import OpenCageQuery, OpenCageResult
