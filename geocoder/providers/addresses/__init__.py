512c944981a1b5d4c6c966a3f7bbcda2539d7287
Andrey Shpak <ashpak@ashpak.ru>
2022-05-23 01:54:57 +0300

    Merge gisgraphy and gisgraphy_reverse and move to core providers

--- geocoder/providers/addresses/__init__.py
+++ geocoder/providers/addresses/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa
-from geocoder.providers.addresses.gisgraphy import GisgraphyQuery, GisgraphyResult
 from geocoder.providers.addresses.mapbox import MapboxQuery, MapboxResult
 from geocoder.providers.addresses.mapzen import MapzenQuery, MapzenResult
 from geocoder.providers.addresses.opencage import OpenCageQuery, OpenCageResult
