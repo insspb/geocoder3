c8d9699f4a2295f5480ab3f914c94aaa4eb401ae
Andrey Shpak <ashpak@ashpak.ru>
2022-05-24 18:43:01 +0300

    Update __all__ in tgos.py

--- geocoder/providers/__init__.py
+++ geocoder/providers/__init__.py
@@ -120,7 +120,7 @@ from geocoder.providers.opencage import (
 )
 from geocoder.providers.osm import OsmQuery, OsmQueryDetail, OsmResult, OsmReverse
 from geocoder.providers.tamu import TamuQuery, TamuResult
-from geocoder.providers.tgos import TgosQuery
+from geocoder.providers.tgos import TgosQuery, TgosResult
 from geocoder.providers.tomtom import TomtomQuery
 from geocoder.providers.uscensus import (
     USCensusBatch,
